02947b6f7265ae8812ca4fa55eed30f2a3c9432c
Teddy Wing <git@teddywing.com>
2024-09-14 17:47:42 +0200

    content.js: Ideas to try to get top window's href from frame
    Going to have to abandon this idea because we can't get around the
    cross-origin problems. The top window's origin is "mail.google.com" and
    the chat frame is at "chat.google.com".
    
    We'd have to set up messaging to the background to get the current URL,
    all for a verification that we're in the right chat frame. Deciding that
    I'm not going to bother verifying against the top window's URL, that
    should be simpler.

--- content.js
+++ content.js
@@ -1,11 +1,11 @@
 function get_space_id () {
-	var url_parts = window.location.href.split('/');
+	var url_parts = window.top.location.href.split('/');
 	var space_id = url_parts[url_parts.length - 1];
 	return space_id;
 }
 
 function space_idtype () {
-	var url_parts = window.location.href.split('/');
+	var url_parts = window.top.location.href.split('/');
 	var space_type = url_parts[url_parts.length - 2];
 	var space_id = url_parts[url_parts.length - 1];
 	return `${space_type}/${space_id}`;
